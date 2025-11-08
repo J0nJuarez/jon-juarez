2fb2e3671a78dd1df97cbac35bb84fafd2b37dd6
jon <jonjuarez.contacto@gmail.com>
2025-11-08 14:03:40 +0100

    Configurar bloque de descripcion´de forma dinamica con props y estilos

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Descripcion from './componentes/descripcion'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="p-4 grid grid-cols-4 grid-rows-5 gap-4 h-dvh">
+        <div className="col-span-2 row-span-3">      
+          <Descripcion texto="Hola, esta es una descripcion." />
+        </div>
+        <div className="col-span-2 row-span-2 col-start-1 row-start-4">2</div>
+        <div className="col-span-2 row-span-5 col-start-3 row-start-1">3</div>
+    </div>
+    
+        
     </>
   )
 }
--- src/componentes/descripcion.tsx
+++ src/componentes/descripcion.tsx
@@ -4,9 +4,18 @@ interface DescripcionProps {
     texto: string;
 }
 
-const Descripcion: React.FC<DescripcionProps> = () => {
+const Descripcion: React.FC<DescripcionProps> = ({ texto }) => {
   return (
-    <div>descripcion</div>
+    <div className='text-left clip-1-2 p-[2rem] flex flex-col'>
+        <div className='ml-[20%]'>
+            <h2 className='text-6xl '>SEO</h2>
+            <h2 className='text-6xl '>WEB</h2>
+            <h2 className='text-6xl '>DEV</h2>
+        </div>
+        <p className='text-left text-lg mt-2 w-[50%] text-white'>
+            {texto}
+        </p>
+    </div>
   )
 }
 
