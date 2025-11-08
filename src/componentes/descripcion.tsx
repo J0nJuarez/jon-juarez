98a757f2134fc92e7665813861b740ad24c59720
jon <jonjuarez.contacto@gmail.com>
2025-11-08 13:09:13 +0100

    Empezar proyecto

---
+++ src/componentes/descripcion.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface DescripcionProps {
+    texto: string;
+}
+
+const Descripcion: React.FC<DescripcionProps> = () => {
+  return (
+    <div>descripcion</div>
+  )
+}
+
+export default Descripcion
